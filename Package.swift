0d1806be8afe9392070c5044cf56dbbe07864450
Bernat <bernatgarciacarbo@gmail.com>
2025-02-26 13:48:56 +0100

    init

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "DoordeckSDK",
-         url: "https://cdn.doordeck.com/xcframework/v0.50.0/DoordeckSDK.xcframework.zip",
-         checksum: "7a2eed8e6c4427245a98b27dd4fbe7be2f6f311fdc0107fcfd7464a1034b1822"
+         url: "https://fff.com/newfile.zip",
+         checksum: "fff"
       )
    ]
 )
